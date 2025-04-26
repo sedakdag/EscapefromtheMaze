753601bafba662cf70fac1c5ea52c92c4e0969df
Yaprak Aslan <yaprakaslan@Yaprak-Air.hgw.local>
2025-04-26 21:53:24 +0300

    turnmanager

--- EscapefromtheMaze/src/com/escape/maze/manager/TurnManager.java
+++ EscapefromtheMaze/src/com/escape/maze/manager/TurnManager.java
@@ -1,5 +1,64 @@
 package com.escape.maze.manager;
+import com.escape.maze.structures.Queue;
+import com.escape.maze.model.Agent;
+import com.escape.maze.structures.Stack;
 
 public class TurnManager {
-	
+	private Queue<Agent> agentQueue;
+    private int currentRound;
+
+    public TurnManager() {
+        agentQueue = new Queue<>();
+        currentRound = 0;
+    }
+    public void add(Agent agent) {
+        agentQueue.enqueue(agent);
+    }
+    public void advanceTurn(){
+        if(agentQueue.isEmpty()){
+            return;
+        }
+        Agent agent = agentQueue.dequeue();
+        boolean reachedGoal = agent.isHasReachedGoal();//geçiçi olarak reachedgoal açtım, agenttaki hasreachedgoal e bağlı
+
+        if (!reachedGoal){
+            agentQueue.enqueue(agent);
+        }
+        currentRound++;
+
+    }
+    public Agent getCurrentAgent() {
+        if(agentQueue.isEmpty()){
+            return null;
+        }
+        return agentQueue.peek();
+    }
+    public boolean allAgentsFinished(){
+        return agentQueue.isEmpty();
+    }
+    //ID,pozisyon,toplam move, power up kullanımı, backtrackler ve son 5 poizyon özeti
+    public void logTurnSummary(Agent agent) {
+        System.out.println("\n>>>>>>>>> TURN " + currentRound + " <<<<<<<<<\n");
+
+        System.out.println("ID of the Agent:" + agent.getId());
+        System.out.println("Current Position:(" + agent.getCurrentX() + ", " + agent.getCurrentY() + ")");
+        System.out.println("Total Moves: " + agent.getTotalMoves());
+        System.out.println("Power Up Active: " + agent.isHasPowerUp());
+        System.out.println("Backtracks: " + agent.getBacktracks());
+
+        System.out.println("Positions of The Last 5 Moves:");
+        Stack<String> temp = new Stack<>(); //temporoary stack oluşturdum
+        int count = 0;
+
+        while (!agent.getMoveHistory().isEmpty() && count < 5) { //move ghistory boş değilse ve 5 adım atmadıysa
+            String move = agent.getMoveHistory().pop(); //son hareketi aldım
+            System.out.println(move);
+            temp.push(move); //temporary kaydettim ki string de silinmesin
+            count++;
+        }
+// move ihstoryden bilgileri aldığım için boşaldı, geri doldurmam gerek
+        while (!temp.isEmpty()) {
+            agent.getMoveHistory().push(temp.pop());
+        }
+    }
 }
