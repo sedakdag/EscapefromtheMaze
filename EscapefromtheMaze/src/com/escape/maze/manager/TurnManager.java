794b735e6828ce6364f9421cdadf7813aa25d01b
Yaprak Aslan <yaprakaslan@Yaprak-Air.hgw.local>
2025-04-29 13:49:10 +0300

    added function to turnmanager changed last 5 moves

--- EscapefromtheMaze/src/com/escape/maze/manager/TurnManager.java
+++ EscapefromtheMaze/src/com/escape/maze/manager/TurnManager.java
@@ -4,38 +4,47 @@ import com.escape.maze.model.Agent;
 import com.escape.maze.structures.Stack;
 
 public class TurnManager {
-	private Queue<Agent> agentQueue;
+    private Queue<Agent> agentQueue;
     private int currentRound;
 
     public TurnManager() {
         agentQueue = new Queue<>();
         currentRound = 0;
     }
+
     public void add(Agent agent) {
         agentQueue.enqueue(agent);
     }
-    public void advanceTurn(){
-        if(agentQueue.isEmpty()){
+
+    public void advanceTurn() {
+        if (agentQueue.isEmpty()) {
             return;
         }
         Agent agent = agentQueue.dequeue();
         boolean reachedGoal = agent.isHasReachedGoal();//i added a temporary reachedgoal, it impoted hasreachedgoal from agent
 
-        if (!reachedGoal){
+        if (!reachedGoal) {
             agentQueue.enqueue(agent);
         }
         currentRound++;
 
     }
+
     public Agent getCurrentAgent() {
-        if(agentQueue.isEmpty()){
+        if (agentQueue.isEmpty()) {
             return null;
         }
         return agentQueue.peek();
     }
-    public boolean allAgentsFinished(){
+
+    public boolean allAgentsFinished() {
         return agentQueue.isEmpty();
     }
+
+    public Queue<Agent> getAgentQueue() {
+        return agentQueue;
+    }
+
     // summary of ID,position ,total moves, if it as used power up , backtracks and  last 5 moves
     public void logTurnSummary(Agent agent) {
         System.out.println("Turn:" + currentRound);
@@ -44,7 +53,7 @@ public class TurnManager {
         System.out.println("Current Position:(" + agent.getCurrentX() + ", " + agent.getCurrentY() + ")");
         System.out.println("Total Moves: " + agent.getTotalMoves());
         System.out.println("Power Up Active: " + agent.isHasPowerUp());
-       //look at the LINES AGAIN THERE MIGHT BE SOMETHING YOU HAVE FORGOTTEN
+        //look at the LINES AGAIN THERE MIGHT BE SOMETHING YOU HAVE FORGOTTEN
         if (agent.isHasPowerUp()) {
             System.out.println("Agent " + agent.getId() + " used a Power Up");
         }
@@ -55,18 +64,30 @@ public class TurnManager {
 
 //i got some help while writing last 5 moves
         System.out.println("Positions of The Last 5 Moves:");
-        Stack<String> temp = new Stack<>(); //temporoary stack oluşturdum
-        int count = 0;
 
-        while (!agent.getMoveHistory().isEmpty() && count < 5) { //if move history is not empty and hast moved 5 times
-            String move = agent.getMoveHistory().pop(); //popped last move
-            System.out.println(move);
-            temp.push(move); //i did it temporary so it doesnt remove it from the stirng
-            count++;
+        Stack<String> originalHistory = agent.getMoveHistory();
+
+
+        // a temp copy stack for moveHistory
+        Stack<String> copy = new Stack<>();
+        Stack<String> temp = new Stack<>();
+
+        while (!originalHistory.isEmpty()) {
+            String move = originalHistory.pop();
+            copy.push(move);
+            temp.push(move);
         }
-// move history is empty since i got the info from it, i have to fill it up again
+
+        // turning moveHistory back to normal
         while (!temp.isEmpty()) {
-            agent.getMoveHistory().push(temp.pop());
+            originalHistory.push(temp.pop());
+        }
+
+        // taking 5 moves from copy
+        int count = 0;
+        while (!copy.isEmpty() && count < 5) {
+            System.out.println(copy.pop());
+            count++;
         }
     }
 }
